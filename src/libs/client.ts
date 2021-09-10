8839385176c07bca71233c47c96f8c885e8b4336
Shun <root@LAPTOP-2I9IL7ET.localdomain>
2021-09-10 11:21:18 +0900

    workflow edited

--- src/libs/client.ts
+++ src/libs/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from "microcms-js-sdk"
 
 export const client = createClient({
-	serviceDomain: process.env.SERVICE_DOMAIN ? process.env.SERVICE_DOMAIN : "",
+	serviceDomain: 'shunyama',
 	apiKey: process.env.API_KEY ? process.env.API_KEY : "",
 })
